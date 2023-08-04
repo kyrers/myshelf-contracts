eb33ddd134160315ab61615fea90784d53e9aae7
kyrers <kyrers.eth@gmail.com>
2023-08-04 19:50:58 +0100

    changeURI test

--- test/BookRepository.t.sol
+++ test/BookRepository.t.sol
@@ -5,6 +5,8 @@ import {Test} from "forge-std/Test.sol";
 import {BookRepository} from "src/BookRepository.sol";
 
 contract BookRepositoryTest is Test {
+    error NotOwner();
+
     //From IERC1155
     event TransferSingle(
         address indexed operator,
@@ -40,5 +42,39 @@ contract BookRepositoryTest is Test {
 
         string memory uri = bookRepository.uri(1);
         assertEq("fake_uri", uri);
+    }
+
+    function testChangeUri() public {
+        vm.prank(bob);
+        vm.expectEmit();
+
+        emit TransferSingle(bob, address(0), bob, 1, 10);
+        bookRepository.publish("fake_uri", 1, 10);
+
+        vm.prank(alice);
+        vm.expectEmit();
+
+        emit TransferSingle(alice, address(0), alice, 2, 10);
+        bookRepository.publish("fake_uri_alice", 2, 10);
+
+        vm.prank(alice);
+
+        bookRepository.changeURI(2, "new_uri_alice");
+
+        string memory aliceURI = bookRepository.uri(2);
+        assertEq("new_uri_alice", aliceURI);
+
+        vm.expectRevert(NotOwner.selector);
+        bookRepository.changeURI(1, "not_owner");
+
+        vm.prank(bob);
+        bookRepository.changeURI(1, "new_uri_bob");
+
+        string memory bobURI = bookRepository.uri(1);
+        assertEq("new_uri_bob", bobURI);
+
+        vm.expectRevert(NotOwner.selector);
+
+        bookRepository.changeURI(2, "not_owner");
     }
 }
